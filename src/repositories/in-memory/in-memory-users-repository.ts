39582d47aafefefe94989c951d6b913d0e655320
chain <klaus.mika1052@gmail.com>
2023-11-08 20:55:41 -0300

    feat: implements users repository in memory

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,34 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-01',
+      name: data.name,
+      last_name: data.last_name,
+      email: data.email,
+      password: data.password,
+      avatar_url: data.avatar_url ?? null,
+      job_level: data.job_level ?? null,
+      tellphone: data.tellphone ?? null,
+      about_me: data.about_me ?? null,
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+}
