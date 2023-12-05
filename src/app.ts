8c42f8e3d7bedca187814918cc5c7d3342093e06
Rafael Sousa <rafiinhaa.sousa@hotmail.com>
2023-12-05 11:45:04 -0300

    add: checking variable

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
 import { usersRoutes } from './http/controllers/users/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(usersRoutes)
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO:  Inplementar ferramenta para obter os logs de erro.
+  }
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
